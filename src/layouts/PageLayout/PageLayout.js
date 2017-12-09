08a8ad517ff951ead7ec3a6ea0c54d66de9688db
eitanritz <eitan.ritz@gmail.com>
2017-12-09 16:11:01 +0200

    font changes and styling

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -7,11 +7,11 @@ export const PageLayout = ({ children }) => (
   <div className='container'>
     <div className='navbar-datr'>
       <div className='datr'>datr</div>
-      <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
-      {' · '}
-      <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
-      {' · '}
-      <Link to='/example' activeClassName='page-layout__nav-item--active'>Example</Link>
+      <div className='nav-items'>
+        <IndexLink to='/' className='page-layout__nav-item' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
+        <Link to='/counter' className='page-layout__nav-item' activeClassName='page-layout__nav-item--active'>Counter</Link>
+        <Link to='/example' className='page-layout__nav-item' activeClassName='page-layout__nav-item--active'>Example</Link>
+      </div>
     </div>
     <div className='page-layout__viewport'>
       {children}
--- src/routes/Example/components/Example.js
+++ src/routes/Example/components/Example.js
@@ -4,10 +4,23 @@ import './Example.scss'
 export const Example = () => (
   <div className="example" >
     <h1>what is datr?</h1>
-    {' '}
-    <p>datr is an online data exploration platform</p>
-    {' '}
-    <p>datr is your first call in getting to know your data</p>
+    
+    <p>Curabitur rutrum tempus ex eu mollis. Nullam sed diam nec magna finibus dictum.
+    Sed rutrum erat quis nisl malesuada, et vestibulum tellus congue.
+    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+    Nulla tempor arcu et nisi efficitur, id convallis nibh ornare.
+    Donec tortor nisi, interdum euismod imperdiet ut, rhoncus nec sapien.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Nullam rhoncus dapibus erat. Aenean viverra rhoncus felis, sit amet aliquam mi fringilla quis.
+    Maecenas vel vestibulum eros, et volutpat turpis. Aenean ultricies accumsan tellus.
+    Etiam a elit sit amet magna tincidunt semper sit amet efficitur libero.
+    Suspendisse volutpat accumsan commodo. Etiam lorem turpis, aliquet sit amet pretium eget, consequat ac elit.
+    Duis imperdiet pretium nunc id vestibulum. Suspendisse sit amet accumsan dolor, vel tincidunt ante.
+    Nam nec erat in ipsum mollis dignissim. Vivamus ultricies mi quis felis lacinia feugiat.
+    Proin mauris magna, pretium et congue eu, imperdiet non nisi. Vestibulum posuere blandit tincidunt.
+    Proin rutrum euismod sapien, quis ullamcorper lorem sodales id.
+    Suspendisse bibendum mauris sit amet viverra feugiat. Cras rutrum eros vel malesuada lacinia.</p>
 
   </div>
 )
