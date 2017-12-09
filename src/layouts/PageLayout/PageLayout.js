391692fe19fee4f2e56997fb6f61784aa7ad4548
micahzev <friedland.micah@gmail.com>
2017-12-09 14:42:31 +0200

    added example route

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types'
 import './PageLayout.scss'
 
 export const PageLayout = ({ children }) => (
-  <div className='container text-center'>
-    <h1>datr</h1>
-    <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
+  <div className='container'>
+    <div className='navbar-datr'>
+      <div className='datr'>datr</div>
+      <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
+      {' · '}
+      <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
+      {' · '}
+      <Link to='/example' activeClassName='page-layout__nav-item--active'>Example</Link>
+    </div>
     <div className='page-layout__viewport'>
       {children}
     </div>
---
+++ src/routes/Example/components/Example.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Example.scss'
+
+export const Example = () => (
+  <div className="example" >
+    <h1>what is datr?</h1>
+    {' '}
+    <p>datr is an online data exploration platform</p>
+    {' '}
+    <p>datr is your first call in getting to know your data</p>
+
+  </div>
+)
+
+export default Example
