c31879a89346686f4d1919e181741333bdc67299
eitanritz <eitan.ritz@gmail.com>
2017-12-11 21:19:49 +0200

    added hero title plus styling

--- src/routes/Example/components/Example.js
+++ src/routes/Example/components/Example.js
@@ -1,28 +1,62 @@
 import React from 'react'
+
+import Blobs from '../assets/Blobs.png'
+
 import './Example.scss'
 
 export const Example = () => (
-  <div className="example" >
-    <h1>what is datr?</h1>
-    
-    <p>Curabitur rutrum tempus ex eu mollis. Nullam sed diam nec magna finibus dictum.
-    Sed rutrum erat quis nisl malesuada, et vestibulum tellus congue.
-    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-    Nulla tempor arcu et nisi efficitur, id convallis nibh ornare.
-    Donec tortor nisi, interdum euismod imperdiet ut, rhoncus nec sapien.</p>
+<div className="example" >
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Nullam rhoncus dapibus erat. Aenean viverra rhoncus felis, sit amet aliquam mi fringilla quis.
-    Maecenas vel vestibulum eros, et volutpat turpis. Aenean ultricies accumsan tellus.
-    Etiam a elit sit amet magna tincidunt semper sit amet efficitur libero.
-    Suspendisse volutpat accumsan commodo. Etiam lorem turpis, aliquet sit amet pretium eget, consequat ac elit.
-    Duis imperdiet pretium nunc id vestibulum. Suspendisse sit amet accumsan dolor, vel tincidunt ante.
-    Nam nec erat in ipsum mollis dignissim. Vivamus ultricies mi quis felis lacinia feugiat.
-    Proin mauris magna, pretium et congue eu, imperdiet non nisi. Vestibulum posuere blandit tincidunt.
-    Proin rutrum euismod sapien, quis ullamcorper lorem sodales id.
-    Suspendisse bibendum mauris sit amet viverra feugiat. Cras rutrum eros vel malesuada lacinia.</p>
-
+  <div className='hero-title'>
+    <h1>visualise your data&rsquo;s potential</h1>
+    <h2>with data exploration techniques you can start to understand and qualify your data</h2>
   </div>
+
+  <img alt='Data Visualisation' className='blobs' src={Blobs} />
+
+
+    <h1>what is datr?</h1>
+    <div className='paragraph-box'>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Nullam rhoncus dapibus erat. Aenean viverra rhoncus felis, sit amet aliquam mi fringilla quis.
+      Maecenas vel vestibulum eros, et volutpat turpis. Aenean ultricies accumsan tellus.
+      Etiam a elit sit amet magna tincidunt semper sit amet efficitur libero.
+      Suspendisse volutpat accumsan commodo. Etiam lorem turpis, aliquet sit amet pretium eget, consequat ac elit.
+      Duis imperdiet pretium nunc id vestibulum. Suspendisse sit amet accumsan dolor, vel tincidunt ante.
+      Nam nec erat in ipsum mollis dignissim. Vivamus ultricies mi quis felis lacinia feugiat.
+      Proin mauris magna, pretium et congue eu, imperdiet non nisi. Vestibulum posuere blandit tincidunt.
+      Proin rutrum euismod sapien, quis ullamcorper lorem sodales id.
+      Suspendisse bibendum mauris sit amet viverra feugiat. Cras rutrum eros vel malesuada lacinia.</p>
+
+      <p>Curabitur rutrum tempus ex eu mollis. Nullam sed diam nec magna finibus dictum.
+      Sed rutrum erat quis nisl malesuada, et vestibulum tellus congue.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      Nulla tempor arcu et nisi efficitur, id convallis nibh ornare.
+      Donec tortor nisi, interdum euismod imperdiet ut, rhoncus nec sapien.</p>
+    </div>
+
+
+    <h1>how does it work?</h1>
+    <div className='paragraph-box'>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Nullam rhoncus dapibus erat. Aenean viverra rhoncus felis, sit amet aliquam mi fringilla quis.
+      Maecenas vel vestibulum eros, et volutpat turpis. Aenean ultricies accumsan tellus.
+      Etiam a elit sit amet magna tincidunt semper sit amet efficitur libero.
+      Suspendisse volutpat accumsan commodo. Etiam lorem turpis, aliquet sit amet pretium eget, consequat ac elit.
+      Duis imperdiet pretium nunc id vestibulum. Suspendisse sit amet accumsan dolor, vel tincidunt ante.
+      Nam nec erat in ipsum mollis dignissim. Vivamus ultricies mi quis felis lacinia feugiat.
+      Proin mauris magna, pretium et congue eu, imperdiet non nisi. Vestibulum posuere blandit tincidunt.
+      Proin rutrum euismod sapien, quis ullamcorper lorem sodales id.
+      Suspendisse bibendum mauris sit amet viverra feugiat. Cras rutrum eros vel malesuada lacinia.</p>
+
+      <p>Curabitur rutrum tempus ex eu mollis. Nullam sed diam nec magna finibus dictum.
+      Sed rutrum erat quis nisl malesuada, et vestibulum tellus congue.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      Nulla tempor arcu et nisi efficitur, id convallis nibh ornare.
+      Donec tortor nisi, interdum euismod imperdiet ut, rhoncus nec sapien.</p>
+    </div>
+
+</div>
 )
 
 export default Example
---
+++ src/routes/Example/components/Visualisation.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Visualisation.scss'
+
+import * as d3 from "d3";
+
+export const Visualisation = () => (
+  <svg></svg>
+)
+
+var svg = d3.select("body").append("svg");
+
+var force = d3.layout.force();
+
+d3.json("graph.json", function(error, graph) {
+  if (error) throw error;
+
+  force
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .on("tick", tick)
+      .start();
+
+  var link = svg.selectAll(".link")
+      .data(graph.links)
+    .enter().append("line")
+      .attr("class", "link");
+
+  var node = svg.selectAll(".node")
+      .data(graph.nodes)
+    .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 4.5);
+
+  resize();
+  d3.select(window).on("resize", resize);
+
+  function tick() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  }
+
+  function resize() {
+    width = window.innerWidth, height = window.innerHeight;
+    svg.attr("width", width).attr("height", height);
+    force.size([width, height]).resume();
+  }
+});
+
+export default Visualisation
